28e24590da91b9c5ebded6cd232a35e671a20b19
chrisupt2001 <73676459+chrisupt2001@users.noreply.github.com>
2023-05-23 00:10:55 -0400

    Delete src directory

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import Container from '../components/Container';
-
-const Home = () => {
-    return (<Container>
-        <p>Home Page TK</p>
-    </Container>)
-}
-
-export default Home;
--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import Container from '../components/Container';
-
-const NotFound = () => {
-    return (<Container>
-        <p>Sorry, but there is no content here...</p>
-    </Container>)
-}
-
-export default NotFound;
